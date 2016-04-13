c77411719084f5a503c650d6880976be1e7997ae
dklementik <klementikdamien@gmail.com>
2016-04-13 14:59:38 +0200

    test 1 route

--- lozapp/urls.py
+++ lozapp/urls.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
+
 from views import eventclick, events, homepage, log, logpart, logpro, post, userpart, userpro
+from django.conf.urls import url, include
+
 
 urlpatterns = [
     url(r'^$', homepage),
-    url(r'^$', eventclick),
-    url(r'^$', events),
-    url(r'^$', log),
-    url(r'^$', logpro),
-    url(r'^$', logpart),
-    url(r'^$', post),
-    url(r'^$', userpro),
-    url(r'^$', userpart),
+    url(r'^eventclick$', eventclick),
+    url(r'^events$', events),
+    url(r'^log$', log),
+    url(r'^logpro$', logpro),
+    url(r'^logpart$', logpart),
+    url(r'^post$', post),
+    url(r'^userpro$', userpro),
+    url(r'^userpart$', userpart),
 ]
