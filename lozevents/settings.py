a51ddbc983e38ad2e21e96ff05a37849c8e51dcc
jladine <ladine.johan@gmail.com>
2016-04-13 16:08:04 +0200

    ajout header

--- lozevents/settings.py
+++ lozevents/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'lozevents.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'lozevents', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
