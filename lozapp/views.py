da87e6cc70e822a36a81687a147ac8d9d638cf78
dklementik <klementikdamien@gmail.com>
2016-05-04 11:18:56 +0200

    Rename and move templates

--- lozapp/views.py
+++ lozapp/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 def homepage(request):
     return render(request, "homepage.html")
 
-def event_display(request):
-    return render(request, "eventdisplay.html")
+def event_display(request, slug):
+    return render(request, "event_display.html")
 
 def event_list(request):
-    return render(request, "eventlist.html")
+    return render(request, "event_list.html")
 
 def event_create(request):
-    return render(request, "eventcreate.html")
+    return render(request, "event_create.html")
--- lozprofile/views.py
+++ lozprofile/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
-def profile_part_display(request):
-    return render(request, "profilepart.html")
+def profile_part_display(request, username):
+    return render(request, "profile_part_display.html")
 
-def profile_pro_display(request):
-    return render(request, "profilepro.html")
+def profile_pro_display(request, username):
+    return render(request, "profile_pro_display.html")
 
 
 #voir avec userena
 def signup_pro(request):
-    return render(request, "userpro.html")
+    return render(request, "signup_pro.html")
 
 def signup_part(request):
-    return render(request, "userpart.html")
+    return render(request, "signup_part.html")
